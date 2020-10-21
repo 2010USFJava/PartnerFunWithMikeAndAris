9929134b6fb97fe4bfcda8cb89b72f4a586f9834
arisolivia <aocarrol@gmail.com>
2020-10-21 17:00:34 -0400

    added driver

---
+++ Driver/src/com/revature/Partner/Driver.java
@@ -0,0 +1,12 @@
+package com.revature.Partner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Starbucks store1 = new Starbucks("123 Main Street","Mike", new Coffee("Vanilla",1234,"medium","01/2021"));
+		System.out.println(store1);
+		System.out.println("Aris Branch");
+
+	}
+
+}
